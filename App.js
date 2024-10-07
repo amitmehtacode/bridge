60318c23a6682e01865d9eeccef0c722e393e088
amitmehtacode <amitkumarttn@gmail.com>
2024-10-07 10:47:26 +0530

    android bridging

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import React, { useEffect } from 'react';
-import {StyleSheet, Text, View, NativeModules} from 'react-native';
-const {CalculatorModule} = NativeModules;
+import {StyleSheet, Text, View, NativeModules, Platform} from 'react-native';
+const { HelloWorld , CalculatorModule} = NativeModules;
 
 const App = () => {
+
   useEffect(() => {
-    CalculatorModule.add(5, 10, (error, sum) => {
-      if (error) {
-        console.error(error);
-      } else {
-        console.log('Sum is:', sum); // Output: Sum is: 15
-      }
-    });
-  }, []);
+    if(Platform.OS === 'android'){
+      HelloWorld?.sayHello()?.then((message) => {
+        console.log('----->>>>>>', message); // Logs: "Hello from Android Native Code!"
+      });
+    } else {
+      CalculatorModule.add(5, 10, (error, sum) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log('Sum is:', sum); // Output: Sum is: 15
+        }
+      });
+    }
+  }, [])
+  
 
   return (
     <View style={styles.container}>
